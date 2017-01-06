1166cee60e141f2abbe9b73a0818d8ab3dd3f9c0
Zhang Hu <zhanghu@yunba.io>
2017-01-06 13:46:22 +0800

    reformat namespace

--- src/TopicTreeNode.cc
+++ src/TopicTreeNode.cc
@@ -7,79 +7,79 @@
 #include "TopicTreeNode.h"
 
 namespace Yunba {
-    std::map<uint64_t, std::set<TopicTreeNode*>> TopicTreeNode::_uid_subcribed_topic;
-    const std::string TopicTreeNode::OFFLINE = "N";
 
-    TopicTreeNode::TopicTreeNode(TopicTreeNode* parent, std::string data) :
-    _parent(parent)
-    , _name(data)
-    {
+std::map<uint64_t, std::set<TopicTreeNode *>> TopicTreeNode::_uid_subcribed_topic;
+const std::string TopicTreeNode::OFFLINE = "N";
+
+TopicTreeNode::TopicTreeNode(TopicTreeNode *parent, std::string data) :
+        _parent(parent), _name(data) {
+}
+
+std::shared_ptr<TopicTreeNode> TopicTreeNode::addChild(const std::string &name, const std::string &data) {
+    auto it = _children.find(name);
+    if (it == _children.end()) {
+        auto child = std::make_shared<TopicTreeNode>(this, name);
+        _children[name] = child;
+        return child;
     }
 
-    std::shared_ptr<TopicTreeNode> TopicTreeNode::addChild(const std::string &name, const std::string &data) {
-        auto it = _children.find(name);
-        if (it == _children.end()) {
-            auto child = std::make_shared<TopicTreeNode>(this, name);
-            _children[name] = child;
-            return child;
-        }
+    return it->second;
+}
 
-        return it->second;
-    }
+void TopicTreeNode::removeChild(const std::string &name) {
+    _children.erase(name);
+}
 
-    void TopicTreeNode::removeChild(const std::string &name) {
-        _children.erase(name);
-    }
-
-    TopicTreeNode* TopicTreeNode::findNode(const std::string &path) {
-        std::vector<std::string> result;
+TopicTreeNode *TopicTreeNode::findNode(const std::string &path) {
+    std::vector<std::string> result;
 //        boost::trim_if(path, boost::is_any_of("\t ")); // split can't omit separators at ends
-        boost::split(result, path, boost::is_any_of("/"), boost::token_compress_on);
+    boost::split(result, path, boost::is_any_of("/"), boost::token_compress_on);
 
-        auto current = this;
-        for(int i=0; i<result.size(); i++) {
-            std::string name = result[i];
-            if (i == 0) {
-                if (name == "") {
-                    name = "/";
-                }
-            }
-
-            // '/' represents the root node
-            if (name == "/") {
-                if (_parent != nullptr) {
-                    return nullptr;
-                } else {
-                    continue;
-                }
-            } else {
-                current = current->getChild(name).get();
-                if (current == nullptr) {
-                    return current;
-                } else {
-                    if (i == result.size() - 1) {
-                        return current;
-                    }
-                }
+    auto current = this;
+    for (int i = 0; i < result.size(); i++) {
+        std::string name = result[i];
+        if (i == 0) {
+            if (name == "") {
+                name = "/";
             }
         }
 
-        return nullptr;
+        // '/' represents the root node
+        if (name == "/") {
+            if (_parent != nullptr) {
+                return nullptr;
+            } else {
+                continue;
+            }
+        } else {
+            current = current->getChild(name).get();
+            if (current == nullptr) {
+                return current;
+            } else {
+                if (i == result.size() - 1) {
+                    return current;
+                }
+            }
+        }
     }
 
-    std::set<uint64_t> TopicTreeNode::getUids() {
-        return _uids;
-    }
+    return nullptr;
+}
 
-    const std::map<uint64_t, std::string> &TopicTreeNode::get_uids_and_route() const {
-        return _uids_and_route;
-    }
+std::set<uint64_t> TopicTreeNode::getUids() {
+    return _uids;
+}
 
-    const std::multimap<std::string, uint64_t> &TopicTreeNode::get_route_index() const {
-        return _route_index;
-    }
+const std::map<uint64_t, std::string> &TopicTreeNode::get_uids_and_route() const {
+    return _uids_and_route;
+}
 
-    const std::map<uint64_t, std::set<TopicTreeNode *>> &TopicTreeNode::get_uid_subcribed_topic() const {
-        return _uid_subcribed_topic;
-    }
+const std::multimap<std::string, uint64_t> &TopicTreeNode::get_route_index() const {
+    return _route_index;
+}
+
+const std::map<uint64_t, std::set<TopicTreeNode *>> &TopicTreeNode::get_uid_subcribed_topic() const {
+    return _uid_subcribed_topic;
+}
+
 }
--- src/TopicTreeNode.h
+++ src/TopicTreeNode.h
@@ -7,94 +7,106 @@
 #include <memory>
 
 namespace Yunba {
-    class TopicTreeNode {
+
+class TopicTreeNode {
 
 //        using TopicTreeNodePtr = std::shared_ptr<TopicTreeNode<T>>;
 //        typedef std::shared_ptr<TopicTreeNode<T>> TopicTreeNodePtr;
-    private:
-        std::string _name;
-        TopicTreeNode* _parent;
-        std::map< std::string, std::shared_ptr<TopicTreeNode> > _children;
-        std::set<uint64_t> _uids;
-        std::map<uint64_t, std::string> _uids_and_route;
-        std::multimap<std::string, uint64_t> _route_index;
-        static std::map<uint64_t, std::set<TopicTreeNode*>> _uid_subcribed_topic;
-    public:
-        const std::map<uint64_t, std::set<TopicTreeNode *>> &get_uid_subcribed_topic() const;
+private:
+    std::string _name;
+    TopicTreeNode *_parent;
+    std::map<std::string, std::shared_ptr<TopicTreeNode> > _children;
+    std::set<uint64_t> _uids;
+    std::map<uint64_t, std::string> _uids_and_route;
+    std::multimap<std::string, uint64_t> _route_index;
+    static std::map<uint64_t, std::set<TopicTreeNode *>> _uid_subcribed_topic;
+public:
+    const std::map<uint64_t, std::set<TopicTreeNode *>> &get_uid_subcribed_topic() const;
 
-    public:
-        const std::map<uint64_t, std::string> &get_uids_and_route() const;
-        const std::multimap<std::string, uint64_t> &get_route_index() const;
+public:
+    const std::map<uint64_t, std::string> &get_uids_and_route() const;
 
-    public:
-        static const std::string OFFLINE;
-        TopicTreeNode(TopicTreeNode* parent, std::string data);
-        ~TopicTreeNode() {};
+    const std::multimap<std::string, uint64_t> &get_route_index() const;
 
-        std::string getName() const {
-            return _name;
-        };
-        void setData(const std::string& data) {
-            _name = data;
-        };
-        std::shared_ptr<TopicTreeNode> addChild(const std::string& name, const std::string& data);
-        void removeChild(const std::string &name);
-        std::shared_ptr<TopicTreeNode> findChild(const std::string& data) const {};
-        std::shared_ptr<TopicTreeNode> getChild(const std::string &name) const {
-            std::shared_ptr<TopicTreeNode> p;
-            try {
-                p = _children.at(name);
-            } catch (const std::out_of_range& oor) {
-                p = nullptr;
-            }
-            return p;
-        };
-        TopicTreeNode* getParent() const { return _parent; };
-        int getNumChildren() const {};
+public:
+    static const std::string OFFLINE;
 
-        TopicTreeNode* findNode(const std::string &path);
+    TopicTreeNode(TopicTreeNode *parent, std::string data);
 
-        int addUid(uint64_t uid, std::string route) {
+    ~TopicTreeNode() {};
 
-            bool remove_old = false;
-            auto uid_to_route = _uids_and_route.find(uid);
-            if (uid_to_route != _uids_and_route.end()) {
-                if (uid_to_route->second != route) {
-                    remove_old = true;
-                }
-            }
-
-            if (remove_old) {
-                std::string& current_route = uid_to_route->second;
-                // remove current <route, uid> pair
-                auto iterpair = _route_index.equal_range(current_route);
-                for (auto it = iterpair.first; it != iterpair.second; it++) {
-                    if (it->second == uid) {
-                        _route_index.erase(it);
-                    }
-                }
-            }
-
-            _route_index.insert(std::pair<std::string, uint64_t>(route, uid));
-            _uids_and_route[uid] = route;
-
-            // update id -> topics
-            auto uid_to_topics = _uid_subcribed_topic.find(uid);
-            if (uid_to_topics == _uid_subcribed_topic.end()) {
-                _uid_subcribed_topic[uid] = std::set<TopicTreeNode*>();
-            }
-            uid_to_topics = _uid_subcribed_topic.find(uid);
-            uid_to_topics->second.insert(this);
-        }
-
-        int addUid(uint64_t uid) {
-            _uids.insert(uid);
-
-            addUid(uid, OFFLINE);
-        }
-
-        std::set<uint64_t> getUids();
+    std::string getName() const {
+        return _name;
     };
+
+    void setData(const std::string &data) {
+        _name = data;
+    };
+
+    std::shared_ptr<TopicTreeNode> addChild(const std::string &name, const std::string &data);
+
+    void removeChild(const std::string &name);
+
+    std::shared_ptr<TopicTreeNode> findChild(const std::string &data) const {};
+
+    std::shared_ptr<TopicTreeNode> getChild(const std::string &name) const {
+        std::shared_ptr<TopicTreeNode> p;
+        try {
+            p = _children.at(name);
+        } catch (const std::out_of_range &oor) {
+            p = nullptr;
+        }
+        return p;
+    };
+
+    TopicTreeNode *getParent() const { return _parent; };
+
+    int getNumChildren() const {};
+
+    TopicTreeNode *findNode(const std::string &path);
+
+    int addUid(uint64_t uid, std::string route) {
+
+        bool remove_old = false;
+        auto uid_to_route = _uids_and_route.find(uid);
+        if (uid_to_route != _uids_and_route.end()) {
+            if (uid_to_route->second != route) {
+                remove_old = true;
+            }
+        }
+
+        if (remove_old) {
+            std::string &current_route = uid_to_route->second;
+            // remove current <route, uid> pair
+            auto iterpair = _route_index.equal_range(current_route);
+            for (auto it = iterpair.first; it != iterpair.second; it++) {
+                if (it->second == uid) {
+                    _route_index.erase(it);
+                }
+            }
+        }
+
+        _route_index.insert(std::pair<std::string, uint64_t>(route, uid));
+        _uids_and_route[uid] = route;
+
+        // update id -> topics
+        auto uid_to_topics = _uid_subcribed_topic.find(uid);
+        if (uid_to_topics == _uid_subcribed_topic.end()) {
+            _uid_subcribed_topic[uid] = std::set<TopicTreeNode *>();
+        }
+        uid_to_topics = _uid_subcribed_topic.find(uid);
+        uid_to_topics->second.insert(this);
+    }
+
+    int addUid(uint64_t uid) {
+        _uids.insert(uid);
+
+        addUid(uid, OFFLINE);
+    }
+
+    std::set<uint64_t> getUids();
+};
+
 }
 
 #endif
